41e130e76feb7545ca7319532b676975649d1568
JaumeKothan <jaumeollerfernandez@gmail.com>
2023-01-30 21:14:26 +0100

    update

--- Pokedex-Website/src/App.jsx
+++ Pokedex-Website/src/App.jsx
@@ -3,39 +3,49 @@ import "./App.css";
 import { Pokemon } from "./pokeComponent/pokemon";
 import { useEffect } from "react";
 
-
-
 function App() {
-  const [name, setName] = useState('');
+  // types/0/type/name
+  const [name, setName] = useState("");
   const [imageURL, setImageURL] = useState(null);
   var [isLoading, setIsLoading] = useState(true);
+  const [type, setType] = useState("");
+  const [selectedColor, setSelectedColor] = useState("");
+  const colorTypes = {
+    fire: "rgb(143, 0, 0)",
+    plant: "rgb(131,186,54)",
+  };
+
+  var PokemonToShow_Number = "6";
+
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon/" + PokemonToShow_Number + "/")
+      .then((response) => response.json())
+      .then((data) => {
+        setName(data.name);
+        setImageURL(data.sprites.front_default);
+        setIsLoading(false);
+        setType(data.types[0].type.name);
+      });
+  }, []);
   
-  var PokemonToShow_Number = "6"
+  if (isLoading) {
+    return (
+      <div>
+        <h1>Cargando</h1>
+      </div>
+    );
+  }
 
-   useEffect(() => {
-      fetch('https://pokeapi.co/api/v2/pokemon/'+ PokemonToShow_Number +'/')
-        .then((response) => response.json())
-        .then((data) => {
-          setName(data.name);
-          setImageURL(data.sprites.front_default)
-          setIsLoading(false);
-        })}, []);
-
-    if(isLoading){
-      return(
-        <div>
-          <h1>Cargando</h1>
-        </div>
-      )
-    }
-
-     return (<div className="App">
-        <Pokemon 
-        name = {name}
-        image = {imageURL}
-        number = {PokemonToShow_Number}
-        />
-      </div>);
+  return (
+    <div className="App">
+      <Pokemon
+        name={name}
+        image={imageURL}
+        number={PokemonToShow_Number}
+        type={type}
+      />
+    </div>
+  );
 }
 
 export default App;
--- Pokedex-Website/src/pokeComponent/pokemon.jsx
+++ Pokedex-Website/src/pokeComponent/pokemon.jsx
@@ -1,12 +1,25 @@
+import { useState } from "react";
 import "./pokemon.css";
 
 export function Pokemon(props) {
 
     return (
-      <div>
-        <h1 className="PokemonName">{props.name}</h1>
-        <img className="PokemonImage" src={props.image} alt={props.name} />
-        <p className="PokemonNumber">{props.number}</p>
+        <div className="pokemonCapsule row">
+            <div className="col">
+                 <img className="PokemonImage" src={props.image} alt={props.name} />
+            </div>
+        <div className="row">
+            <div className="col">
+            <h2 className="PokemonName">{props.name}</h2>
+            </div>
+        </div>
+        <div className="row">
+            <div className="col">
+            <h3 className="PokemonNumber">{props.number}</h3>
+            </div>
+        </div>
+
+
       </div>
     );
   }
