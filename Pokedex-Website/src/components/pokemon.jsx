a5960f47db0a8f0c4425c3e08fa3d78629946284
JaumeKothan <jaumeollerfernandez@gmail.com>
2023-02-02 20:59:37 +0100

    at least modal works

--- Pokedex-Website/src/components/pokemon.jsx
+++ Pokedex-Website/src/components/pokemon.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import PokeModal from "./modal/pokemodal";
 import "./pokemon.css";
 
 export function Pokemon(props) {
@@ -9,40 +11,55 @@ export function Pokemon(props) {
     ev.target.src = "./src/assets/badges/notfound.png";
   };
 
-  return (
-    <div className="m-2 card" style={{ backgroundColor: props.color }}>
-      <img
-        className="card-img-top"
-        onError={BrokenImage}
-        src={props.image}
-        alt={props.name}
-      />
-      <div className="card-body text-center">
-        <h2 className="PokemonName">{props.name}</h2>
-      </div>
+  const [showModal, setShowModal] = useState(false);
 
-      <div className="card-text">
-        <div className="col text-center">
-          <img
-            className="py-2 px-1"
-            src={props.BadgeType}
-            onError={BrokenBadge}
-            width={70}
-            alt=""
-            srcset=""
-          />
-          {props.lengthType === 2 ? (
+  return (
+    <>
+      {showModal ? (
+        <PokeModal
+          onClose={() => {
+            setShowModal(false);
+          }}
+        />
+      ):console.log('error')}
+      <div
+        className="m-2 card"
+        onClick={()=>{setShowModal(true)}}
+        style={{ backgroundColor: props.color }}
+      >
+        <img
+          className="card-img-top"
+          onError={BrokenImage}
+          src={props.image}
+          alt={props.name}
+        />
+        <div className="card-body text-center">
+          <h2 className="PokemonName">{props.name}</h2>
+        </div>
+
+        <div className="card-text">
+          <div className="col text-center">
             <img
               className="py-2 px-1"
-              src={props.BadgeType2}
+              src={props.BadgeType}
               onError={BrokenBadge}
               width={70}
               alt=""
               srcset=""
             />
-          ) : null}
+            {props.lengthType === 2 ? (
+              <img
+                className="py-2 px-1"
+                src={props.BadgeType2}
+                onError={BrokenBadge}
+                width={70}
+                alt=""
+                srcset=""
+              />
+            ) : null}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
